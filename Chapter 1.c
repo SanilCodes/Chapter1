715a8c48a42715fff269c49dbc3934c4b7571d67
SanilCodes <sanil.landge07@gmail.com>
2024-12-29 11:43:56 +0530

    Add files via upload

---
+++ Chapter 1.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int main(){
+
+
+    //Variables Topic:
+    /*
+    int number = 25;
+    char star = '*';
+    int age = 16;
+    float pi = 3.14;
+    */
+
+    //DataTypes Topic:
+    /*
+    int weight = 72;
+    float height = 5.10;
+    char at = '@';
+    */
+
+    //Output Topic:
+    /*
+    printf("Hello C \n");
+    printf("Hello C \n");
+    printf("Hello C \n");
+    printf("Hello C \n");
+
+    printf("age is %d \n", age );
+    printf("value of %f \n", pi );
+    printf("star looks like this %c \n", star );
+    */
+    
+    //Input Topic:
+    /*
+    int num;
+    printf("Enter Age \n");
+    scanf("%d", &num);
+    printf("age is : %d", &num);
+    
+
+    int a, b;
+
+    printf("Enter a");
+    scanf("%d",&a);
+
+    printf("Enter b");
+    scanf("%d",&b);
+
+    printf("a is %d\n", a);
+    printf("b is %d\n", b);
+
+    int c = a+b;
+    printf("sum of a and b is %d", c);
+    //This also can be written without making a seperate variable 'c' as below
+    // printf("sum of a and b is %d", a + b);
+
+    */
+
+   //Practice Q 1
+   /*
+   int num;
+   printf("Enter a Number");
+   scanf("%d",&num);
+    printf("square of your number is %d", num * num);
+    */
+
+   //Practice Q 2
+   /*
+    int rad;
+    printf("Enter the Radius");
+    scanf("%d", &rad);
+    printf("Area of Circle is %d", 3.14 * rad * rad );
+    */
+
+
+
+    return 0;
+}
+
+
